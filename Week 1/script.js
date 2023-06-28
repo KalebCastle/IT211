8529dbb1c434875cc5f23b02b38d0efa332721eb
Kaleb Castle <kalebm.castle@gmail.com>
2023-06-28 12:43:15 -0700

    completed assigniment

--- Week 1/script.js
+++ Week 1/script.js
@@ -1,1 +1,4 @@
-
+let firstName = "Kaleb";
+let lastName = "Castle";
+let yearsOfStudy = "3 months";
+let goal = " pursue a career in tech, I hope to begin in a QA role and work my way to a PM role";
