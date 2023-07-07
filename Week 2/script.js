6f87815b8e9358046a86d03980a03dff7d9d9957
Kaleb Castle <kalebm.castle@gmail.com>
2023-07-07 00:14:56 -0700

    Added alert and personal comment at the top of JS

--- Week 2/script.js
+++ Week 2/script.js
@@ -1,6 +1,17 @@
+//HW2 Pig Latin, Kaleb Castle (I did use ChatGPT to help we understand some concepts so not all code is my orginal ideas but it was all typed by me), worked on from 7/5/2023 - 7/7/2023
 // Function to translate text to Pig Latin
 function translateToPigLatin() {
   var inputText = document.getElementById("inputText").value; // Get input text from textarea
+
+  // Check if input text is empty or contains only single-letter words
+  if (inputText.trim() === '' || /^[a-z]\b/i.test(inputText)) {
+    document.getElementById("errorMessage").textContent = "Please enter valid text to translate.";
+    document.getElementById("outputText").textContent = ""; // Clear any previously translated text
+    return;
+  }
+
+  document.getElementById("errorMessage").textContent = ""; // Clear error message
+
   var words = inputText.split(" "); // Split input into individual words
   var translatedText = ""; // Variable to store translated text
 
@@ -22,7 +33,7 @@ function translateToPigLatin() {
     translatedText += pigLatinWord + " "; // Append translated word to the translated text
   }
 
-  document.getElementById("outputText").innerHTML = translatedText.trim(); // Display translated text
+  document.getElementById("outputText").textContent = translatedText.trim(); // Display translated text
 }
 
 // Add event listener to translate button
